639f15ac8ad671a7c5522fd9249063e2e5b1be85
roc198 <roc198@gmail.com>
2016-10-27 12:10:49 +0800

    init

---
+++ models/article.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Article = new Schema({
+    title: String,
+    author: String,
+    publisher:String,
+    procuratorate: {type: mongoose.Schema.Types.ObjectId, ref: 'Procuratorate'},
+    content: String,
+    articleType: {type: mongoose.Schema.Types.ObjectId, ref: 'ArticleType'},
+    comment: String,
+    readNums: Number,
+    isExpired: Boolean
+});
+
+module.exports = mongoose.model('Article', Article);
