ad729050259b7857c077f87cd1ece57111b49a9a
xiaoxi <494273025@qq.com>
2016-11-08 10:57:07 +0800

    检察院

--- routes/article.js
+++ routes/article.js
@@ -5,19 +5,21 @@ var ArticleType = require('../models/articletype');
 var Procuratorate = require('../models/procuratorate');
 /* GET users listing. */
 
-//文章类型的存储
+//文章类型表的存储
 router.get('/type', function(req, res, next) {
-    res.render('articletype');
+    res.render('articletype', { title: '文章类型发表' });
 })
 
 router.post('/type', function(req, res, next) {
     var typeName = req.body.articletype;
+
     var description = req.body.description;
     if (typeName == undefined || typeName == "") {
         res.render(error);
     }
     var articleType = new ArticleType({
         typeName: typeName,
+
         description: description
     });
     articleType.save(function(err, articletype) {
@@ -26,6 +28,12 @@ router.post('/type', function(req, res, next) {
     });
 })
 
+router.get('/typeList', function(req, res, next) {
+    ArticleType.find({}, function(err, articletypes) {
+        res.render('type-list', { title: '类型列表', articletypes: articletypes })
+    })
+
+})
 
 //获取发表文章页面
 router.get('/add', function(req, res, next) {
@@ -60,7 +68,7 @@ router.post('/add', function(req, res, next) {
     article.save(function(err, articles) {
         if (err) console.log(err);
         console.log(articles);
-        res.redirect('/article/list');
+        res.redirect('/article/add');
     })
 })
 
@@ -111,4 +119,119 @@ router.post('/update', function(req, res, next) {
         res.redirect('/article/list');
     })
 })
+
+
+//本院简介
+router.get('/introduction', function(req, res, next) {
+    Article
+        .find({})
+        .populate('articleType')
+        .exec(function(err, articles) {
+            var Articles = [];
+            for (var i = 0; i < articles.length; i++) {
+                if (articles[i].articleType.typeName == '本院简介') {
+                    Articles.push(articles[i]);
+
+                }
+            }
+            res.render('introduction', { title: '本院简介', Articles: Articles });
+        })
+})
+
+// 一级菜单 "检察快讯",
+router.get('/news', function(req, res, next) {
+    Article
+        .find({})
+        .populate('articleType procuratorate')
+        .exec(function(err, articles) {
+            var Articles = [];
+            for (var i = 0; i < articles.length; i++) {
+                if (articles[i].articleType.typeName == '检察快讯') {
+                    Articles.push(articles[i]);
+
+                }
+            }
+            res.render('news', { title: '检察快讯', Articles: Articles });
+        })
+});
+
+//检察快讯子菜单
+router.get('/news_details1', function(req, res, next) {
+    var _id = req.query._id;
+    Article
+        .findOne({ _id: _id })
+        .populate('articleType procuratorate')
+        .exec(function(err, Article) {
+            res.render('news_details1', { title: '检察快讯', Article: Article });
+        })
+})
+router.get('/news_details2', function(req, res, next) {
+    var _id = req.query._id;
+    Article
+        .findOne({ _id: _id })
+        .populate('articleType procuratorate')
+        .exec(function(err, Article) {
+            res.render('news_details1', { title: '检察快讯', Article: Article });
+        })
+})
+router.get('/news_details3', function(req, res, next) {
+    var _id = req.query._id;
+    Article
+        .findOne({ _id: _id })
+        .populate('articleType procuratorate')
+        .exec(function(err, Article) {
+            res.render('news_details1', { title: '检察快讯', Article: Article });
+        })
+})
+router.get('/news_details4', function(req, res, next) {
+    var _id = req.query._id;
+    Article
+        .findOne({ _id: _id })
+        .populate('articleType procuratorate')
+        .exec(function(err, Article) {
+            res.render('news_details1', { title: '检察快讯', Article: Article });
+        })
+})
+router.get('/news_details5', function(req, res, next) {
+    var _id = req.query._id;
+    Article
+        .findOne({ _id: _id })
+        .populate('articleType procuratorate')
+        .exec(function(err, Article) {
+            res.render('news_details1', { title: '检察快讯', Article: Article });
+        })
+})
+
+//一级菜单'阳光百姓工程'
+
+router.get('/sunPeople', function(req, res, next) {
+    Article
+        .find({})
+        .populate('articleType procuratorate')
+        .exec(function(err, articles) {
+            var Articles1 = [];
+            var Articles2 = [];
+            var Articles3 = [];
+            for (var i = 0; i < articles.length; i++) {
+                if (articles[i].articleType.typeName == '基础建设') {
+                    Articles1.push(articles[i]);
+
+                }
+                if (articles[i].articleType.typeName == '惠农补贴') {
+                    Articles2.push(articles[i]);
+
+                }
+                if (articles[i].articleType.typeName == '领域建设') {
+                    Articles3.push(articles[i]);
+
+                }
+            }
+            res.render('sunPeople', { title: '阳光百姓工程', Articles1: Articles1, Articles2: Articles2, Articles3: Articles3 });
+        })
+})
+
+//一级菜单'预防教育'
+
+
+
 module.exports = router;
